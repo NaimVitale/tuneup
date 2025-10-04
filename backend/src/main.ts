7a1e7d11eebead3983b1f10bae9ac3472fad3381
NaimVitale <lucasnaimvitale04@gmail.com>
2025-10-04 16:39:26 +0200

    Activation of CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: ['https://tuneup-blush.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
