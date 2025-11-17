31207117a96fdbac8c3ad29d511b8e3c1f363ceb
NaimVitale <lucasnaimvitale04@gmail.com>
2025-11-17 02:57:29 +0100

    Fix a vulnerability on Tickets

--- backend/src/entradas/entrada.controller.ts
+++ backend/src/entradas/entrada.controller.ts
@@ -1,5 +1,5 @@
 // src/entradas/entrada.controller.ts
-import { Controller, Get, Post, Body, Param, Query, NotFoundException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query, NotFoundException, UseGuards, Req, ForbiddenException } from '@nestjs/common';
 import { EntradaService } from './entrada.service';
 import { JwtRolesGuard } from 'src/auth/jwt-roles.guard';
 
@@ -10,7 +10,10 @@ export class EntradaController {
   // Listar todas las entradas o filtradas por id_usuario (via query)
   @UseGuards(JwtRolesGuard)
   @Get()
-  async findAll(@Query('id_usuario') id_usuario?: number, @Query('limit') limit?: string) {
+  async findAll(@Query('id_usuario') id_usuario?: number, @Query('limit') limit?: string, @Req() req?: any,) {
+    if (id_usuario && req.user.id !== Number(id_usuario)) {
+      throw new ForbiddenException('No tienes permiso para ver estas entradas');
+    }
     return this.entradaService.findAllByUsuario(id_usuario, limit ? Number(limit) : undefined);
   }
 }
